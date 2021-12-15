df23e36c17d2f7d7bb6998f59b80c5085e5e1cfe
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-15 16:17:43 +0100

    filterBookList function

--- m4-d3-d4/src/components/BookList.jsx
+++ m4-d3-d4/src/components/BookList.jsx
@@ -1,19 +1,46 @@
 import SingleBook2 from "./SingleBook2"
-import { Container, Row } from 'react-bootstrap'
+import { Component } from 'react'
+import { Container, Row, Form, Button } from 'react-bootstrap'
 import books from '../data/history.json'
 
-const BookList = (props) => {
-    return (
-        <Container>
-            <Row className="justify-content-around">
-                {
-                    books.map(book => (
-                        <SingleBook2 book={book}/>
-                    ))
-                }
-            </Row>
-        </Container>
-    )
+class BookList extends Component {
+    state = {
+        search:""
+    }
+    render() {
+        return (
+            <>
+            <Container>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Enter book</Form.Label>
+                        <Form.Control
+                        type="text"
+                        placeholder="Enter book"
+                        onChange={(e) => {
+                            this.setState({
+                                search: e.target.value
+                            })
+                        }} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Search
+                    </Button>
+                </Form>
+            </Container>
+            
+            <Container>
+                <Row className="justify-content-around">
+                    {
+                        books.map(book => (
+                            <SingleBook2 book={book}/>
+                        ))
+                    }
+                </Row>
+            </Container>
+            </>
+        )
+    }
 }
 
 export default BookList
